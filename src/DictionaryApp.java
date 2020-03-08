0c4e0bae0d649f3a9245ea1557fea92a7a480c4c
dvaykhanskiy <dvaykhanskiy@ucvts.org>
2020-03-08 14:18:05 -0400

    added some buttons, they dont do much yet

--- src/DictionaryApp.java
+++ src/DictionaryApp.java
@@ -37,10 +37,15 @@ import javax.swing.JTextPane;
 import javax.swing.DropMode;
 import java.awt.FlowLayout;
 import javax.swing.BoxLayout;
+import javax.swing.AbstractAction;
+import javax.swing.Action;
 
 public class DictionaryApp {
 
 	private JFrame frmDic;
+	private JTextField txtSearch;
+	private final Action add_action = new Add();
+	private final Action remove_action = new Remove();
 
 	/**
 	 * Launch the application.
@@ -69,9 +74,44 @@ public class DictionaryApp {
 	 * Initialize the contents of the frmDic.
 	 */
 	private void initialize() {
-		frmDic = new JFrame();
+		frmDic = new JFrame("Dictionary");
 		frmDic.setBounds(100, 100, 450, 300);
 		frmDic.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmDic.getContentPane().setLayout(null);
+		
+		JButton btnAdd = new JButton("Add");
+		btnAdd.setAction(add_action);
+		btnAdd.setBounds(0, 0, 77, 29);
+		frmDic.getContentPane().add(btnAdd);
+		
+		JButton btnRemove = new JButton("Remove");
+		btnRemove.setAction(remove_action);
+		btnRemove.setBounds(67, 0, 77, 29);
+		frmDic.getContentPane().add(btnRemove);
+		
+		txtSearch = new JTextField();
+		txtSearch.setText("Search:");
+		txtSearch.setBounds(10, 30, 130, 26);
+		frmDic.getContentPane().add(txtSearch);
+		txtSearch.setColumns(10);
 	}
 
+	private class Add extends AbstractAction {
+		public Add() {
+			putValue(NAME, "Add");
+			putValue(SHORT_DESCRIPTION, "Adds a word");
+		}
+		public void actionPerformed(ActionEvent e) {
+			System.out.println("add");
+		}
+	}
+	private class Remove extends AbstractAction {
+		public Remove() {
+			putValue(NAME, "Remove");
+			putValue(SHORT_DESCRIPTION, "Removes a word");
+		}
+		public void actionPerformed(ActionEvent e) {
+			System.out.println("remove");
+		}
+	}
 }
