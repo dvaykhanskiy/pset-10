8c52df813b77ff384af7ac692fcb731dbd523134
dvaykhanskiy <dvaykhanskiy@ucvts.org>
2020-03-08 12:43:42 -0400

    renamed frame

--- src/DictionaryApp.java
+++ src/DictionaryApp.java
@@ -4,7 +4,7 @@ import javax.swing.JFrame;
 
 public class DictionaryApp {
 
-	private JFrame frame;
+	private JFrame frmDic;
 
 	/**
 	 * Launch the application.
@@ -14,7 +14,7 @@ public class DictionaryApp {
 			public void run() {
 				try {
 					DictionaryApp window = new DictionaryApp();
-					window.frame.setVisible(true);
+					window.frmDic.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -30,12 +30,12 @@ public class DictionaryApp {
 	}
 
 	/**
-	 * Initialize the contents of the frame.
+	 * Initialize the contents of the frmDic.
 	 */
 	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmDic = new JFrame();
+		frmDic.setBounds(100, 100, 450, 300);
+		frmDic.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 
 }
