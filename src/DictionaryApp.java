9e07a0d129fc4e6c966e0e4da91a9ddabc1c0587
dvaykhanskiy <dvaykhanskiy@ucvts.org>
2020-03-11 23:03:44 -0400

    ah yes, code

--- src/DictionaryApp.java
+++ src/DictionaryApp.java
@@ -192,8 +192,7 @@ public class Dictionay {
 		rdbtnNewRadioButton_1.setBounds(110, 78, 59, 23);
 		frmDictionary.getContentPane().add(rdbtnNewRadioButton_1);
 		rdbtnNewRadioButton_1.addItemListener(new ItemListener() {
-//			 select asc or desc order
-		    @Override
+
 		    public void itemStateChanged(ItemEvent event) {
 		    	
 		        int state = event.getStateChange();
